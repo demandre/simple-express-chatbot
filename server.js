e9e903d0967fb32d1812ed7af2e8ed1d616ab13f
Joris Demandre <joris.demandre@gmail.com>
2019-09-25 10:42:46 +0200

    add process default port to server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const PORT = process.env.PORT || 3000
+
 const app = express()
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-    console.log('Hello world listening on port 3000')
+app.listen(PORT, function () {
+    console.log('Hello world listening on port ' + PORT)
 })
