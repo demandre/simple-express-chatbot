944f8b8da7536bf266e7f0c604e06058cd989b9f
Joris Demandre <joris.demandre@gmail.com>
2019-09-25 11:41:33 +0200

    use name get parameter to return custom hello

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
+app.get('/hello', function (req, res) {
+    let name = req.query.name
+    if(typeof name !== 'undefined') {
+        res.send('Hello ' + name + '!')
+    } else {
+        res.send('Whats your name?')
+    }
+})
+
 app.listen(PORT, function () {
     console.log('Hello world listening on port ' + PORT)
 })
